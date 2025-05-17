025f2c5a82a68b2a42f4148882f62001b96a183f
Manuel Nicastro <nicastrogm13@gmail.com>
2025-05-17 00:49:59 -0300

    feat(help): add tip for using 'unipm add' command
    This commit adds a helpful tip to the help content, encouraging users to try the 'unipm add' command for installing packages. This enhancement aims to improve user experience by providing guidance on package installation.

--- src/commands/contents/help.ts
+++ src/commands/contents/help.ts
@@ -4,4 +4,5 @@ unipm v[#variables.version]
 [#text.green][#text.bold]Available commands:[#text.reset]
 [#variables.commandsTable]
 
+[#text.yellow][#text.italic]Tip: try using the 'unipm add' command to install a package.[#text.reset]
 `;
